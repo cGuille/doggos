ec17dc3445acba93f32416505e829707f164ccbe
Guillaume Charmetant <cguille.dev@gmail.com>
2018-07-21 19:12:42 +0200

    implement endpoints w/o state

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest};
 
-fn index(_req: HttpRequest) -> &'static str {
-    "Hello world!"
+#[macro_use] extern crate serde_derive;
+use actix_web::{server, http, App, Path, Json};
+
+#[derive(Deserialize)]
+struct Doggo {
+    id: String,
+    name: String,
+}
+
+fn register_doggo(doggo: Json<Doggo>) -> String {
+    format!("Welcome {}! Good boy.", doggo.name)
+}
+
+fn fetch_doggo(path: Path<(String,)>) -> String {
+    format!("Hello again, good boy {}!", path.0)
 }
 
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind("127.0.0.1:8088")
-        .unwrap()
-        .run();
+    let server = server::new(||
+        App::new()
+            .resource("/doggos", |r| r.method(http::Method::POST).with(register_doggo))
+            .resource("/doggos/{doggo_id}", |r| r.method(http::Method::GET).with(fetch_doggo))
+    );
+
+    server.bind("127.0.0.1:8088").unwrap().run();
 }
