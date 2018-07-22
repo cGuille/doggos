d7aa9bee85a8cc9df4aa2878247b3066e3957035
Guillaume Charmetant <cguille.dev@gmail.com>
2018-07-22 10:28:31 +0200

    add in-memory repository

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
 extern crate actix_web;
-
 #[macro_use] extern crate serde_derive;
-use actix_web::{server, http, App, Path, Json};
 
-#[derive(Deserialize)]
+use actix_web::{server, http, App, Path, Json};
+use std::collections::HashMap;
+
+#[derive(Clone,Deserialize)]
 struct Doggo {
     id: String,
     name: String,
+}
+
+struct InMemoryDoggoRepository {
+    map: HashMap<String, Doggo>,
+}
+
+impl InMemoryDoggoRepository {
+    pub fn new() -> InMemoryDoggoRepository {
+        InMemoryDoggoRepository {
+            map: HashMap::new(),
+        }
+    }
+
+    fn save(&mut self, doggo: Doggo) {
+        self.map.insert(doggo.id.to_owned(), doggo.to_owned());
+    }
+
+    fn find(&self, doggo_id: &String) -> Option<Doggo> {
+        match self.map.get(doggo_id) {
+            None => None,
+            Some(doggo) => Some(doggo.to_owned()),
+        }
+    }
 }
 
 fn register_doggo(doggo: Json<Doggo>) -> String {
